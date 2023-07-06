1cd2ded31b9fd9c80785f193a7b3455ac5b9f94e
JoseBatista <batistdev@gmail.com>
2023-07-06 00:10:49 -0300

    feat: add anuidade patente

---
+++ Models/Anuidade.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GestaoPI.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoPI.Models
+{
+    [Table("anuidade")]
+    public class Anuidade
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("ano")]
+        [Range(3, 20)]
+        public short Ano { get; set; }
+
+        [Column("ordinario")]
+        public DateTime Orinario { get; set; }
+
+        [Column("extraordinario")]
+        public DateTime Extraordinario { get; set; }
+
+        public PagamentoAnuidade PagamentoAnuidade { get; set; } = null!;
+    }
+}
---
+++ Models/PagamentoAnuidade.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestaoPI.Models
+{
+    [Table("pagamento_anuidade")]
+    public class PagamentoAnuidade
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("dt_pagamento")]
+        public DateTime DataPagamento { get; set; } = DateTime.Now;
+
+        [Column("nm_protocolo")]
+        public string? NumeroProtocolo { get; set; }
+
+        [Column("vl_pago")]
+        public decimal ValorPago { get; set; }
+
+        [Column("anotacao")]
+        public string? Anotacao { get; set; }
+    }
+}
